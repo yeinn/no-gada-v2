e6d87b4195cbec3cff59edab5d605b210da1fda6
Yein Choi <54708042+yeinn@users.noreply.github.com>
2025-05-02 14:54:25 +0900

    🔧UI 개선

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
+
 import './index.css';
 import Home from './pages/Home';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Toaster position="top-right" />
     <Home />
   </StrictMode>
 );
